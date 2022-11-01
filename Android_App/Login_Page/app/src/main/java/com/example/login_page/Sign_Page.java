3ac733d93cab8f8f7ba3cb509a729c3c6ba32c70
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-11-01 20:58:52 +0900

    Login_Page, Sign_Page -> 100%

---
+++ Android_App/Login_Page/app/src/main/java/com/example/login_page/Sign_Page.java
@@ -0,0 +1,19 @@
+package com.example.login_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Sign_Page extends AppCompatActivity {
+
+    EditText txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_page);
+
+
+    }
+}
